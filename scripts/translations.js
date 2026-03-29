8c882a367aec28767667ec0c13f410fa32549e5c
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-03-28 21:37:33 -0300

    feat - creation of the portfolio translation option

---
+++ scripts/translations.js
@@ -0,0 +1,88 @@
+const translations = {
+    pt: {
+        "perfil-descricao": "xxxxxxxxxxxxx xxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxx xxxxx xxxxxxxx xxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxx xxxx xxxxxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx.",
+
+        "titulo-tecnologias": "Tecnologias",
+
+        "dica-filtro": "(Clique em uma para filtrar os projetos)",
+
+        "titulo-projetos": "Projetos",
+
+        "limpar_filtro_tooltip": "Limpar Filtro",
+
+        "expandir_recolher_btn": "Expandir/Recolher",
+
+        "titulo-projeto-1": "Projeto 1",
+
+        "resumo-projeto-1": "xxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxx xxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxx xxxxxx.",
+
+        "T1-Projeto1": "XXXXXX XXXXX XXXX",
+
+        "P1-Projeto1": "xxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxx xxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxxxx xxx xxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxx xxxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxx.",
+
+        "titulo-projeto-2": "Projeto 2",
+
+        "resumo-projeto-2": "xxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxx xxxxxx xxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxx.",
+
+        "T1-Projeto2": "XXXXXXXX XXXXXXX",
+
+        "P1-Projeto2": "xxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxx xxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxx xxxxxxxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxx xxxxxxxx xxxxxxxxxxxxxxxxxx xxxxxxxxx.",
+
+        "titulo-projeto-3": "Projeto 3",
+
+        "resumo-projeto-3": "xxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxxxxxxx xxxx.",
+
+        "T1-Projeto3": "XXXXX XXXXXX XXXX",
+
+        "P1-Projeto3": "xxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxx xxxxx xxxxxxxxxxxx xxxxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxx xxxxxxxxxx xxxxxxxxxxxxxxxxx xxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxx xxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxx xxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxx xxxxxxxxxxxx xxxxxxxx.",
+
+        "formacao": "Formação",
+
+        "curso": "XXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXX",
+
+        "por": "Desenvolvido por "
+    },
+    en: {
+        "perfil-descricao": "yyyyyyyyyyyyy yyyyyy yyyyyyyyyy yyyyyyyyyyyyyyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyy yyyyy yyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyyyy yyyyyyyyyy.",
+
+        "titulo-tecnologias": "Technologies",
+
+        "dica-filtro": "(Click one to filter the projects)",
+
+        "titulo-projetos": "Projects",
+
+        "limpar_filtro_tooltip": "Clear Filter",
+
+        "expandir_recolher_btn": "Expand/Collapse",
+
+        "titulo-projeto-1": "Project 1",
+
+        "resumo-projeto-1": "yyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyy yyyyyy.",
+
+        "T1-Projeto1": "YYYYYY YYYYY YYYY",
+
+        "P1-Projeto1": "yyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyy yyyyyy yyyyyyy yyyyyyy yyyyyyy yyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyyyyyyyyyyy yyyyyyyyyyy yyyyyyyyyyyyy.",
+
+        "titulo-projeto-2": "Project 2",
+
+        "resumo-projeto-2": "yyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyy yyyyyyyy yyyyyyyy yyyyyyy.",
+
+        "T1-Projeto2": "YYYYYYYY YYYYYYY",
+
+        "P1-Projeto2": "yyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyy yyyyyyyy yyyyyyyy yyyyyyy yyyyyyy yyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyyyyyyyyy yyyyyyyyyyyyy yyyyyyyy yyyyyyyyyyyyyy yyyyyyyyyy yyyyyyyyyyy yyyyyy yyy yyyyyyyyyyy yyyyyy.",
+
+        "titulo-projeto-3": "Project 3",
+
+        "resumo-projeto-3": "yyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyyyy yyyyyyyyyy yyyyyyyy yyyyyyy yyyyyyy yyyyyyyy yyyyyyyyyyyyyy yyyyyy yyyyyyy yyyyyyy yyyyyyyyyyyyyy.",
+
+        "T1-Projeto3": "YYYYY YYYYYY YYYY",
+
+        "P1-Projeto3": "yyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyyy yyyyyyyy yyyyyyy yyyyyyy yyyyyyyy yyyyyyyy yyyyyyy yyyyyy yyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyy yyyyyyyyyy yyyyyyyyyy yyyyyyyyyyyyyyy yyyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyyyyyy yyyyyyy yyyyyy yyyyyyyyyyyy yyyyy yyyyyyyy yyyyyyyyyyyy yyyyyyyyy yyyyyyyyyy yyyyyyyyy yyyyyyyyy.",
+
+        "formacao": "Education",
+
+        "curso": "YYYYYYY YYYYYYYYYYYYY YYYYYYYYYYYY",
+
+        "por": "Developed by "
+    }
+}
